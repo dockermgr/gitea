8521ff6245e45ea65c716491cd3f1389c9e0f11d
Jason <jason@casjay.net>
2021-01-29 23:27:58 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
 APPNAME="gitea"
-DATADIR="/srv/docker/$APPNAME"
 
 mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-docker pull gitea/gitea:latest && docker restart $APPNAME
+  docker pull gitea/gitea:latest && docker restart "$APPNAME"
 else
-docker run -d \
---name "$APPNAME" \
---privileged \
--v $DATADIR:/data \
--p 3000:3000 \
--p 7822:7822 \
---restart=always \
-gitea/gitea:latest
+  docker run -d \
+    --name "$APPNAME" \
+    --privileged \
+    -v "$DATADIR":/data \
+    -p 3000:3000 \
+    -p 7822:7822 \
+    --restart=always \
+    gitea/gitea:latest
 fi

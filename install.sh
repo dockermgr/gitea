5b6e885f7a89b9484c50283d095f9025770c94bd
Jason <jason@casjay.net>
2021-07-10 15:54:14 -0400

    🐛 FIXED: install.sh 🐛

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
 APPNAME="gitea"
+DOCKER_HUB_URL="gitea/gitea:latest"
 
-mkdir -p "$DATADIR" && chmod -Rf 777 "$DATADIR"
+sudo mkdir -p "$DATADIR"
+sudo chmod -Rf 777 "$DATADIR"
 
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
-  docker pull gitea/gitea:latest && docker restart "$APPNAME"
+  sudo docker pull "$DOCKER_HUB_URL"
+  sudo docker restart "$APPNAME"
 else
-  docker run -d \
+  sudo docker run -d \
     --name "$APPNAME" \
     --privileged \
     -v "$DATADIR":/data \
     -p 3000:3000 \
     -p 7822:7822 \
     --restart=always \
-    gitea/gitea:latest
+    "$DOCKER_HUB_URL"
 fi

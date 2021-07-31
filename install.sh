8a7ef82630e9b87764acacc1eaf95506a86f8b56
Jason <jason@casjay.net>
2021-07-31 12:47:52 -0400

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -1,21 +1,72 @@
 #!/usr/bin/env bash
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 APPNAME="gitea"
+VERSION="202107311147-git"
+USER="${SUDO_USER:-${USER}}"
+HOME="${USER_HOME:-${HOME}}"
+SRC_DIR="${BASH_SOURCE%/*}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#set opts
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version       : 202107311147-git
+# @Author        : casjay
+# @Contact       : casjay
+# @License       : WTFPL
+# @ReadME        : dockermgr --help
+# @Copyright     : Copyright: (c) 2021 casjay, casjay
+# @Created       : Saturday, Jul 31, 2021 11:47 EDT
+# @File          : template
+# @Description   : Template for dockermgr
+# @TODO          :
+# @Other         :
+# @Resource      :
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Import functions
+CASJAYSDEVDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}"
+SCRIPTSFUNCTDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}/functions"
+SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-testing.bash}"
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/dfmgr/installer/raw/$GIT_DEFAULT_BRANCH/functions}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if [ -f "$PWD/$SCRIPTSFUNCTFILE" ]; then
+  . "$PWD/$SCRIPTSFUNCTFILE"
+elif [ -f "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" ]; then
+  . "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE"
+else
+  echo "Can not load the functions file: $SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" 1>&2
+  exit 1
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# user system devenv dfmgr dockermgr fontmgr iconmgr pkmgr systemmgr thememgr wallpapermgr
+system_install
+__options "$@"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Begin installer
+APPNAME="${APPNAME:-gitea}"
 DOCKER_HUB_URL="gitea/gitea:latest"
-
-sudo mkdir -p "$DATADIR"
+APPDIR="/usr/local/share/docker/$APPNAME"
+INSTDIR="/usr/local/share/CasjaysDev/$SCRIPTS_PREFIX/$APPNAME"
+DATADIR="/srv/docker/$APPNAME"
+REPORAW="$REPO/raw/$REPO_BRANCH"
+APPVERSION="$(__appversion "$REPORAW/version.txt")"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+sudo mkdir -p "$DATADIR/data"
 sudo chmod -Rf 777 "$DATADIR"
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
   sudo docker pull "$DOCKER_HUB_URL"
   sudo docker restart "$APPNAME"
 else
   sudo docker run -d \
-    --name "$APPNAME" \
+    --name="$APPNAME" \
+    --hostname "$APPNAME" \
     --privileged \
-    -v "$DATADIR":/data \
+    -v "$DATADIR/data":/data \
     -p 3000:3000 \
     -p 7822:7822 \
     --restart=always \
     "$DOCKER_HUB_URL"
 fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# End script
+exit $?

8f8baf93b21a777707ed2b840f23c15b61b7497f
casjay <casjay@yahoo.com>
2021-08-26 10:27:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ HOME="${USER_HOME:-${HOME}}"
 SRC_DIR="${BASH_SOURCE%/*}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #set opts
+exit 1
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ##@Version       : 202107311147-git
@@ -40,49 +41,61 @@ fi
 # user system devenv dfmgr dockermgr fontmgr iconmgr pkmgr systemmgr thememgr wallpapermgr
 dockermgr_install
 __options "$@"
+__sudo() { if sudo -n true; then eval sudo "$*"; else eval "$*"; fi; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Begin installer
 APPNAME="gitea"
 DOCKER_HUB_URL="gitea/gitea"
+GITEA_SERVER_PORT="${GITEA_SERVER_PORT:-3000}"
+GITEA_SERVER_HOST="${GITEA_SERVER_HOST:-$(hostname -f 2>/dev/null)}"
+REPO="${DOCKERMGRREPO:-https://github.com/dockermgr}/$APPNAME"
+REPO_BRANCH="${GIT_REPO_BRANCH:-main}"
+GITEA_SERVER_TIMEZONE="${TZ:-${TIMEZONE:-America/New_York}}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-APPDIR="/usr/local/share/CasjaysDev/$SCRIPTS_PREFIX/$APPNAME"
-INSTDIR="/usr/local/share/CasjaysDev/$SCRIPTS_PREFIX/$APPNAME"
-DATADIR="/srv/docker/$APPNAME"
-REPORAW="$REPO/raw/$GIT_DEFAULT_BRANCH"
+if user_is_root; then
+  APPDIR="$CASJAYSDEVDIR/$SCRIPTS_PREFIX/$APPNAME"
+  INSTDIR="$CASJAYSDEVDIR/$SCRIPTS_PREFIX/$APPNAME"
+  DATADIR="/srv/docker/$APPNAME"
+else
+  APPDIR="$HOME/.local/share/CasjaysDev/$SCRIPTS_PREFIX/$APPNAME"
+  INSTDIR="$HOME/.local/share/CasjaysDev/$SCRIPTS_PREFIX/$APPNAME"
+  DATADIR="$HOME/.local/share/srv/docker/$APPNAME"
+fi
+REPORAW="$REPO/raw/$REPO_BRANCH"
 APPVERSION="$(__appversion "$REPORAW/version.txt")"
-TIMEZONE="${TZ:-$TIMEZONE}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-sudo mkdir -p "$DATADIR"/{data,config}
-sudo chmod -Rf 777 "$DATADIR"
+__sudo mkdir -p "$DATADIR/data"
+__sudo mkdir -p "$DATADIR/config"
+__sudo chmod -Rf 777 "$DATADIR"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-if [ -f "$INSTDIR/docker-compose.yml" ]; then
+if [ -f "$INSTDIR/docker-compose.yml" ] && cmd_exists docker-compose; then
   printf_blue "Installing containers using docker compose"
   sed -i "s|REPLACE_DATADIR|$DATADIR" "$INSTDIR/docker-compose.yml"
   if cd "$INSTDIR"; then
-    sudo docker-compose pull &>/dev/null
-    sudo docker-compose up -d &>/dev/null
+    __sudo docker-compose pull &>/dev/null
+    __sudo docker-compose up -d &>/dev/null
   fi
 else
-  if docker ps -a | grep -qs "$APPNAME"; then
-    sudo docker pull "$DOCKER_HUB_URL" &>/dev/null
-    sudo docker restart "$APPNAME" &>/dev/null
+  if docker ps -a | grep -qsw "$APPNAME"; then
+    __sudo docker pull "$DOCKER_HUB_URL" &>/dev/null
+    __sudo docker restart "$APPNAME" &>/dev/null
   else
-    sudo docker run -d \
-      --name "$APPNAME" \
+    __sudo docker run -d \
+      --name="$APPNAME" \
       --hostname "$APPNAME" \
       --restart=unless-stopped \
       --privileged \
-      -e TZ=${TIMEZONE:-America/New_York} \
+      -e TZ="$GITEA_SERVER_TIMEZONE" \
       -v "$DATADIR/data":/data:z \
       -v "$DATADIR/config":/etc/gitea:z \
-      -p 3000:3000 \
       -p 7822:7822 \
+      -p "$GITEA_SERVER_PORT":3000 \
       "$DOCKER_HUB_URL" &>/dev/null
   fi
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if docker ps -a | grep -qs "$APPNAME"; then
-  printf_blue "Service is available at: http://$HOSTNAME:3000"
+  printf_blue "Service is available at: http://$GITEA_SERVER_HOST:$GITEA_SERVER_PORT"
 else
   false
 fi
